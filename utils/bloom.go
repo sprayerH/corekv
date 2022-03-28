b44c3ceaef49c8a5d0b0b1e4de7a3762bbb0d48f
sprayer <huangfuconghhh@qq.com>
2022-03-28 19:10:40 +0800

    finished bloom filter

--- utils/bloom.go
+++ utils/bloom.go
@@ -14,6 +14,8 @@
 
 package utils
 
+import "math"
+
 // Filter is an encoded set of []byte keys.
 type Filter []byte
 
@@ -28,6 +30,23 @@ func (f Filter) MayContain(h uint32) bool {
 	//Implement me here!!!
 	//在这里实现判断一个数据是否在bloom过滤器中
 	//思路大概是经过K个Hash函数计算，判读对应位置是否被标记为1
+	if len(f) < 2 {
+		return false
+	}
+	k := f[len(f)-1]
+	if k > 30 {
+		return true
+	}
+	nBits := uint32(8 * (len(f) - 1))
+	delta := h>>17 | h<<15
+	for j := uint8(0); j < k; j++ {
+		bitPos := h % nBits
+		if f[bitPos/8]&(1<<(bitPos%8)) == 0 {
+			return false
+		}
+		h += delta
+	}
+
 	return true
 }
 
@@ -35,29 +54,88 @@ func (f Filter) MayContain(h uint32) bool {
 // the given number of bits per key, approximately.
 //
 // A good bitsPerKey value is 10, which yields a filter with ~ 1% false
-// positive rate.
+// positive rate. means m / n
 func NewFilter(keys []uint32, bitsPerKey int) Filter {
 	return Filter(appendFilter(keys, bitsPerKey))
 }
 
 // BloomBitsPerKey returns the bits per key required by bloomfilter based on
-// the false positive rate.
+// the false positive rate. Given n and fp
 func BloomBitsPerKey(numEntries int, fp float64) int {
 	//Implement me here!!!
 	//阅读bloom论文实现，并在这里编写公式
 	//传入参数numEntries是bloom中存储的数据个数，fp是false positive假阳性率
-	return 0
+	// m为位数组大小 n为插入的元素个数 此函数返回值乘以ln2才是真正的k 即hash函数个数
+	m := -1 * float64(numEntries) * math.Log(fp) / math.Pow(0.69314718056, 2)
+	locs := math.Ceil(m / float64(numEntries))
+	return int(locs)
 }
 
 func appendFilter(keys []uint32, bitsPerKey int) []byte {
 	//Implement me here!!!
 	//在这里实现将多个Key值放入到bloom过滤器中
-	return make([]byte, 0)
+	if bitsPerKey < 0 {
+		bitsPerKey = 0
+	}
+
+	// k is num of hash function
+	k := uint32(float64(bitsPerKey) * 0.69)
+	if k < 1 {
+		k = 1
+	}
+	if k > 30 {
+		k = 30
+	}
+
+	nBits := len(keys) * int(bitsPerKey)
+	if nBits < 64 {
+		nBits = 64
+	}
+
+	nBytes := (nBits + 7) / 8
+	nBits = nBytes * 8
+	filter := make([]byte, nBytes+1)
+
+	for _, h := range keys {
+		delta := h>>17 | h<<15
+		for j := uint32(0); j < k; j++ {
+			bitPos := h % uint32(nBits)
+			filter[bitPos/8] |= 1 << (bitPos % 8)
+			h += delta
+		}
+	}
+
+	// save k at end of filter array
+	filter[nBytes] = uint8(k)
+
+	return filter
 }
 
 // Hash implements a hashing algorithm similar to the Murmur hash.
 func Hash(b []byte) uint32 {
 	//Implement me here!!!
 	//在这里实现高效的HashFunction
-	return 0
+	const (
+		seed = 0xbc9f1d34
+		m    = 0xc6a4a793
+	)
+	h := uint32(seed) ^ uint32(len(b))*m
+	for ; len(b) >= 4; b = b[4:] {
+		h += uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
+		h *= m
+		h ^= h >> 16
+	}
+	switch len(b) {
+	case 3:
+		h += uint32(b[2]) << 16
+		fallthrough
+	case 2:
+		h += uint32(b[1]) << 8
+		fallthrough
+	case 1:
+		h += uint32(b[0])
+		h *= m
+		h ^= h >> 24
+	}
+	return h
 }
